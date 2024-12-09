5119dffe1a34f39500f09e300ee0309eee9cd90c
Md Asjad <109430048+asjad895@users.noreply.github.com>
2024-12-09 02:19:24 +0000

    updated

--- Conversational_ChatBot/config/model_config.py
+++ Conversational_ChatBot/config/model_config.py
@@ -2,5 +2,5 @@ MODEL_CONFIGS = ["meta-llama/Llama-3.2-11B-Vision-Instruct","mistralai/Mistral-7
 
 MODEL_SAVE_DIR = '../loaded_model/'
 
-QUANT_FILE = "Llama-3.2-3B-Instruct-Q6_K_L.gguf"
+QUANT_FILE = "Llama-3.2-3B-Instruct-IQ3_M.gguf"
 # Llama-3.2-3B-Instruct-IQ3_M.gguf
